38a1d2a199d11fc5c883e60c7c6a1999a7dbcd91
Max Kamerov <indigowar.github@gmail.com>
2024-04-18 12:45:32 +0300

    added explorer group to which-key

--- .config/nvim/lua/indigowar/plugins/which-key.lua
+++ .config/nvim/lua/indigowar/plugins/which-key.lua
@@ -6,12 +6,13 @@ return {
 		which_key.setup()
 
 		-- Document key chains
-		which_key.register{
-			['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-			['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-			['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-			['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-			['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-		}
+		which_key.register({
+			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			["<leader>e"] = { name = "[E]explore", _ = "which_key_ignore" },
+		})
 	end,
 }
