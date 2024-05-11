0d133d3709ce8b5c0694a63d2a8cd9a14468c967
indigowar <a10levsion@gmail.com>
2024-05-11 21:48:43 +0300

    some changes

--- .config/nvim/lua/indigowar/plugins/themes.lua
+++ .config/nvim/lua/indigowar/plugins/themes.lua
@@ -36,4 +36,32 @@ return {
 
 	{ "rose-pine/neovim", name = "rose-pine" },
 	{ "sainnhe/sonokai" },
+
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				transparent = true,
+				italic_comments = true,
+				hide_fillchars = true,
+				borderless_telescope = true,
+				terminal_colors = true,
+				theme = {
+					variant = "light",
+					highlights = {
+						Comment = { fg = "#696969", bg = "NONE", italic = true },
+					},
+
+					colors = {
+						bg = "#000000",
+						green = "#00ff00",
+						magenta = "#ff00ff",
+					},
+				},
+			})
+			-- vim.cmd("colorscheme cyberdream") -- set the colorscheme
+		end,
+	},
 }
